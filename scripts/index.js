b7e6b5f3b4b880f73a5b957cf92c7565a336c540
asqry <theoneandonlyseek@gmail.com>
2022-07-09 23:43:27 -0500

    feedback

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,16 @@ let staffSelect = document.querySelector('#id-input');
 let feedback = document.querySelector('#feedback');
 let submit = document.querySelector('.submit');
 let userId = document.querySelector('#userId');
+let yourId = document.querySelector('.yourId');
+let form = document.querySelector('.form');
+let warning = document.querySelector('.warning');
 
 let id = new URL(window.location.href).searchParams.get('id');
-if (!id || id == 'null') {
-  let newId = prompt('What is your Discord ID?');
-  if (!newId || newId == null || newId.length < 0) window.location = window.location.href;
+if (!id || id == 'null' || id.length < 17) {
+  yourId.style.display = 'none';
+  form.style.display = 'none';
 
-  let url = new URL(window.location.href);
-  url.searchParams.set('id', newId);
-
-  window.location = url.href;
+  warning.innerText = 'You can only leave feedback if a link is generated for you. \n\n Please ask a staff member to generate a link for you.';
 }
 
 userId.innerText = id;
